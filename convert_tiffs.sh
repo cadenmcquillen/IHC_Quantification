303a101cd71a7a8b8b62765db9d3875a380cd049
Caden McQuillen <54229205+cadenmcquillen@users.noreply.github.com>
2023-06-28 12:15:24 -0400

    Update convert_tiffs.sh

--- convert_tiffs.sh
+++ convert_tiffs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 conda activate py38dabquant
-DIR="/Users/Caden/Desktop/Pou2f3_Data/IHC_Images/6wk_5858_IHC_quant/"
-cd $DIR
-mkdir converted_tiffs
+ImageDIR="/Users/Caden/Desktop/Pou2f3_Data/IHC_Images/6wk_5858_IHC_quant/"
+DabQ_DIR="/Users/Caden/Desktop/Pou2f3_Data/IHC_Images/DAB-quant"
+cd $DabQ_DIR
+mkdir $ImageDIR/converted_tiffs
 
-for file in *.tif; do
-	vips tiffsave $file $DIR/converted_tiffs/new_$file --tile --pyramid --compression deflate --tile-width 256 --tile-height 256
+for file in $ImageDIR/*.tif; do
+	vips tiffsave $file $ImageDIR/converted_tiffs/new_$file --tile --pyramid --compression deflate --tile-width 256 --tile-height 256
 
 done
 
 cd converted_tiffs
 
-python quantify.py --folder=$DIR/converted_tiffs --reprocess --ext=.tif
+python quantify.py --folder=$ImageDIR/converted_tiffs --reprocess --ext=.tif
 
 
